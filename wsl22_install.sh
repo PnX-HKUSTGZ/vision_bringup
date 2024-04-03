bbfd1d9333bacd29725b78b364b31b89c711c902
NeoZeng <neozng@foxmail.com>
2024-04-03 15:14:17 +0800

    [update] add interactive installation script

--- wsl22_install.sh
+++ wsl22_install.sh
@@ -1,30 +1,49 @@
-# change source
+#!/bin/bash
+
+# check internet connection
+ping -c1 -w2 baidu.com > /dev/null
+if [[ $? != 0 ]];then     
+    echo "No internet connection. Try to connect to the internet first then run this script again."
+    exit 0
+else
+    echo "Internet connected."
+fi
+
+echo -e "\e[32mATTENTION:\e[0m"
+echo "during the installation, you may need to input your password for sudo command"
+echo "you also need to input 'y' or 'n' for some options"
+read -p "NOW, press [q/Q] to quit, [enter] or [y/Y] to proceed: " input
+if [[ "$input" =~ ^[qQ]$ ]]; then
+    echo "Exiting..."
+    exit 0
+elif [[ "$input" =~ ^[yY]$ || -z "$input" ]]; then
+    echo "Proceeding..."
+else
+    echo "Invalid input. Exiting..."
+    exit 1
+fi
+
+# change apt source
 sudo mv /etc/apt/sources.list /etc/apt/sources.list.bake
-sudo sh -c "echo '# 默认注释了源码镜像以提高 apt update 速度，如有需要可自行取消注释
-deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy main restricted universe multiverse
-# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy main restricted universe multiverse
+sudo sh -c "echo 'deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy main restricted universe multiverse
 deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-updates main restricted universe multiverse
-# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-updates main restricted universe multiverse
 deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-backports main restricted universe multiverse
-# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-backports main restricted universe multiverse
-
 deb http://security.ubuntu.com/ubuntu/ jammy-security main restricted universe multiverse
-# deb-src http://security.ubuntu.com/ubuntu/ jammy-security main restricted universe multiverse
-
 # 预发布软件源，不建议启用
-# deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-proposed main restricted universe multiverse
-# # deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-proposed main restricted universe multiverse' > /etc/apt/sources.list"
+# deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-proposed main restricted universe multiverse' > /etc/apt/sources.list"
 
-sudo apt update
-sudo apt upgrade -y
+echo -e "\e[32minstall basic tools...\e[0m"
+sudo apt update && sudo apt upgrade -y
 sudo apt install gedit gcc-12 g++-12 cutecom libopencv-dev libopencv-contrib-dev libceres-dev libeigen3-dev build-essential gdb git cmake htop curl gnupg2 wget -y
 
-# install ros
+# install ros2
+echo -e "\e[32minstall ROS2...\e[0m"
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key  -o /usr/share/keyrings/ros-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] https://mirrors.tuna.tsinghua.edu.cn/ros2/ubuntu jammy main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
-sudo apt update
-sudo apt install ros-humble-desktop ros-dev-tools -y
-echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
+sudo apt update && sudo apt install ros-humble-desktop ros-dev-tools -y
+if ! grep -q "source /opt/ros/humble/setup.bash" ~/.bashrc; then
+    echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
+fi
 # 手动模拟 rosdep init
 sudo mkdir -p /etc/ros/rosdep/sources.list.d/
 sudo curl -o /etc/ros/rosdep/sources.list.d/20-default.list https://mirrors.tuna.tsinghua.edu.cn/github-raw/ros/rosdistro/master/rosdep/sources.list.d/20-default.list
@@ -32,32 +51,87 @@ sudo curl -o /etc/ros/rosdep/sources.list.d/20-default.list https://mirrors.tuna
 export ROSDISTRO_INDEX_URL=https://mirrors.tuna.tsinghua.edu.cn/rosdistro/index-v4.yaml
 sudo apt install python3-rosdep2 -y
 rosdep update
-# 每次 rosdep update 之前，均需要增加该环境变量
-# 为了持久化该设定，可以将其写入 .bashrc 中，例如
-echo 'export ROSDISTRO_INDEX_URL=https://mirrors.tuna.tsinghua.edu.cn/rosdistro/index-v4.yaml' >> ~/.bashrc
-echo 'source ~/rmvision/install/setup.bash' >> ~/.bashrc
+# 每次 rosdep update 之前，均需要增加该环境变量. 为了持久化该设定，将其写入 .bashrc 中
+if ! grep -q "export ROSDISTRO_INDEX_URL=https://mirrors.tuna.tsinghua.edu.cn/rosdistro/index-v4.yaml" ~/.bashrc; then
+    echo 'export ROSDISTRO_INDEX_URL=https://mirrors.tuna.tsinghua.edu.cn/rosdistro/index-v4.yaml' >> ~/.bashrc
+fi
 
-# add some convinient alias
-echo "alias cb='colcon build --symlink-install --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=Yes'
-alias mdc='mkdir build && cd build'
-alias cl='cd ..'
-alias rd='rm -r'
-alias md='mkdir'
-" >> ~/.bashrc
+# add some convinient alias,prevent duplicate alias
+if ! grep -q "alias mdc='mkdir build && cd build'" ~/.bashrc; then
+    echo "alias cb='colcon build --symlink-install'
+    alias mdc='mkdir build && cd build'
+    alias mdb='mkdir build && cd build && cmake .. && make -j'
+    alias c.='cd ..'
+    alias c..='cd ../..'
+    alias rd='rm -r'
+    alias md='mkdir -pv'
+    alias h=history
+    alias g1='git clone --depth 1'
+    " >> ~/.bashrc
+fi
 
-# sudo dpkg -i MvCamCtrlSDK_Runtime-4.3.0_x86_64_20231225 -y
-sudo dpkg -i MVS-2.1.2_x86_64_20231225.deb 
-sudo dpkg -i code_1.87.2-1709912201_amd64.deb 
 
-cd 
-source .bashrc
-mkdir -p rmvision/src && cd rmvision/src
-git clone https://github.com/PnX-HKUSTGZ/auto-aim.git --depth 1
-git clone https://github.com/PnX-HKUSTGZ/vision_bringup.git --depth 1
-git clone --depth 1  https://github.com/PnX-HKUSTGZ/rm_serial_driver.git
-git clone --depth 1  https://github.com/PnX-HKUSTGZ/rm_gimbal_description.git
-git clone --depth 1  https://github.com/PnX-HKUSTGZ/hik-driver.git
-cd ..
-rosdep install --from-paths src --ignore-src -r -y
-colcon build --symlink-install --packages-up-to rm_vision_bringup
-source ~/rmvision/install/setup.bash
+# remove ln from sh->dash, use sh->bash
+sudo rm /bin/sh
+sudo ln -s /bin/bash /bin/sh
+
+# install dependencies from .deb files
+echo -e "\e[33mNow we turn to deb installation. Make sure these .deb(s) are in current dir \e[0m"
+read -p "press [Y/y] to install or any other key to skip : " input
+if [[ ! "$input" =~ ^[yY]$ ]]; then
+    echo "Skip deb installation..."
+else
+    read -p $'\e[32m install Hik MVS tools[M/m], just runtime pack[R/r], do not install any[N/n]: \e[0m' install
+    if [ "$install" == "M" ] || [ "$install" == "m" ]; then
+        sudo dpkg -i MVS-2.1.2_x86_64_20231225.deb 
+    elif [ "$install" == "R" ] || [ "$install" == "r" ]; then
+        sudo dpkg -i MvCamCtrlSDK_Runtime-4.3.0_x86_64_20231225 -y
+    else
+        echo "Skipping Hik MVS tools installation."
+    fi
+
+    read -p $'\e[32m install NoMachine remote desktop? (y/n): \e[0m' install
+    if [ "$install" == "y" ]; then
+        sudo dpkg -i nomachine_8.11.3_4_amd64.deb
+    else
+        echo "Skipping NoMachine installation."
+    fi
+
+    read -p $'\e[32m install VSCode? (y/n): \e[0m' install
+    if [ "$install" == "y" ]; then
+        sudo dpkg -i code-server_4.22.1_amd64.deb
+        sudo dpkg -i code_1.87.2-1709912201_amd64.deb
+    else
+        echo "Skipping VSCode installation."
+    fi
+fi
+
+# pull and build rm_vision
+read -p $'\e[32mPull the latest code and build? (y/n): \e[0m' pull_code
+if [ "$pull_code" == "y" ]; then
+    cd ~
+    source ~/.bashrc
+
+    if [ -d "pnx_ws" ]; then
+        echo -e "\e[34m'pnx_ws' directory already exists. Assuming the code has been pulled.\e[0m"
+        cd pnx_ws/src
+    else
+        echo "cloning the latest code..."
+        mkdir -p pnx_ws/src && cd pnx_ws/src
+        git clone https://github.com/PnX-HKUSTGZ/auto-aim.git --depth 1
+        git clone https://github.com/PnX-HKUSTGZ/vision_bringup.git --depth 1
+        git clone --depth 1  https://github.com/PnX-HKUSTGZ/rm_serial_driver.git
+        git clone --depth 1  https://github.com/PnX-HKUSTGZ/rm_gimbal_description.git
+        git clone --depth 1  https://github.com/PnX-HKUSTGZ/hik-driver.git
+    fi
+    
+    cd ..
+    echo "building..."
+    rosdep install --from-paths src --ignore-src -r -y
+    colcon build --symlink-install --packages-up-to rm_vision_bringup
+    source ~/pnx_ws/install/setup.bash
+    echo 'source ~/pnx_ws/install/setup.bash' >> ~/.bashrc
+fi
+
+echo "well done"
+echo "To dev in the terminal, please run 'source ~/.bashrc' to enable the environment"

732bca99223ffd3d1156ae4aad042ce77d41443e
Zijun-Mo <zmo729@connect.hkust-gz.edu.cn>
2025-08-28 17:28:12 +0800

    [update]加入了ai_detector的参数

--- rm_vision_bringup/launch/vision_bringup.launch.py
+++ rm_vision_bringup/launch/vision_bringup.launch.py
@@ -58,7 +58,7 @@ def generate_launch_description():
                     package='armor_detector',
                     plugin='rm_auto_aim::ArmorDetectorNode',
                     name='armor_detector',
-                    parameters=[node_params],
+                    parameters=[node_params, {'use_ai_detector': False}],
                     extra_arguments=[{'use_intra_process_comms': True}]
                 )
     
